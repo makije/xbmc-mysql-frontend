0bc69863eeeb3b19a5cd37012967ae1195976b51
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-12-30 19:13:39 +0100

    Add information about the music

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 	Currently there is {{ Movie::all()->count() }} <a href="movie">movies</a> in {{ MovieSet::all()->count() }} <a href="movieset">sets</a><br/>
-	Currently there is {{ TVShow::all()->count() }} <a href="tvshow">tv shows</a> with {{ Episode::all()->count() }} episodes
+	Currently there is {{ TVShow::all()->count() }} <a href="tvshow">tv shows</a> with {{ Episode::all()->count() }} episodes<br/>
+	Current there is {{ Artist::all()->count() }} <a href="artist">artist</a> with {{ Album::all()->count() }} <a href="album">albums</a> containing {{ Song::all()->count() }} songs
 
 @stop
