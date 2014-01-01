6b289916b479714916aa580ff4f88315a625888b
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-01-01 20:29:05 +0100

    Change the index page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
 
-	Currently there is {{ Movie::all()->count() }} <a href="movie">movies</a> in {{ MovieSet::all()->count() }} <a href="movieset">sets</a><br/>
-	Currently there is {{ TVShow::all()->count() }} <a href="tvshow">tv shows</a> with {{ Episode::all()->count() }} episodes<br/>
-	Current there is {{ Artist::all()->count() }} <a href="artist">artist</a> with {{ Album::all()->count() }} <a href="album">albums</a> containing {{ Song::all()->count() }} songs
+	<h1>Database stats</h1><br/>
+	{{ Actor::all()->count() }} <a href="actor">actors</a><br/>
+	{{ Movie::all()->count() }} <a href="movie">movies</a> in {{ MovieSet::all()->count() }} <a href="movieset">sets</a><br/>
+	{{ TVShow::all()->count() }} <a href="tvshow">tv shows</a> with {{ Episode::all()->count() }} episodes<br/>
+	{{ Artist::all()->count() }} <a href="artist">artist</a> with {{ Album::all()->count() }} <a href="album">albums</a> containing {{ Song::all()->count() }} songs
 
 @stop
