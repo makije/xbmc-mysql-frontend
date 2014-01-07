2edf66ba5b66c4693a5550f29352b752ccff63b1
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-01-07 16:03:33 +0100

    Revert the new movies since last login, last login will be updated before showing the page.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,16 +6,6 @@
 	{{ Person::all()->count() }} <a href="person">persons</a><br/>
 	{{ Movie::all()->count() }} <a href="movie">movies</a> in {{ MovieSet::all()->count() }} <a href="movieset">sets</a><br/>
 	{{ TVShow::all()->count() }} <a href="tvshow">tv shows</a> with {{ Episode::all()->count() }} episodes<br/>
-	{{ Artist::all()->count() }} <a href="artist">artists</a> with {{ Album::all()->count() }} <a href="album">albums</a> containing {{ Song::all()->count() }} songs<br/><br/>
+	{{ Artist::all()->count() }} <a href="artist">artists</a> with {{ Album::all()->count() }} <a href="album">albums</a> containing {{ Song::all()->count() }} songs
 
-	<?php
-		$newMovies = Movie::whereHas('file', function($q)
-		{
-			$q->where('dateAdded', '>', Auth::user()->last_login);
-		})->count();
-	?>
-
-	@if($newMovies > 1)
-		There is {{ $newMovies }} <a href="/movie/latest">new</a> movies since last login
-	@endif
 @stop
