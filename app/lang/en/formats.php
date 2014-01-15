f85987ba1f9f87931104448e4bd67d10812c169f
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-01-15 19:45:24 +0100

    Add some formats

---
+++ app/lang/en/formats.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	/* Video formats */
+	'h264' => 'h264',
+	'hdmv' => 'Blu-ray',
+	'bdmv' => 'Blu-ray',
+	'mpeg2video' => 'DVD',
+	'xvid' => 'XviD',
+
+	/* Audio formats */
+	'ac3' => 'Dolby Digital',
+	'dca' => 'DTS',
+	'dtshd_ma' => 'DTS-HD Master Audio',
+	'mp3' => 'MP3',
+	'aac' => 'Advanced Audio Coding',
+	'vorbis' => 'Vorbis',
+);
