91cdeb2ad564cf400c5165a450497fff468650dd
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-01-15 19:48:22 +0100

    Add more scopes

--- app/models/StreamDetail.php
+++ app/models/StreamDetail.php
@@ -6,4 +6,18 @@ class StreamDetail extends Eloquent {
 	protected $table = 'streamdetails';
 	protected $primaryKey = 'idFile';
 
+	public function scopeVideo($query)
+	{
+		return $query->where('iStreamType', '=', 0);
+	}
+
+	public function scopeAudio($query)
+	{
+		return $query->where('iStreamType', '=', 1)->orderBy('strAudioLanguage');
+	}
+
+	public function scopeSubtitle($query)
+	{
+		return $query->where('iStreamType', '=', 2)->orderBy('strSubtitleLanguage');
+	}
 }
