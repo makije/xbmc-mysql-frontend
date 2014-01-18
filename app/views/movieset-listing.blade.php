d921ad572c9a3ffa24967dddb6dd480d5b2ef054
Martin Kiforenko Jensby <martin@rootuser.dk>
2014-01-18 16:42:43 +0100

    Use the poster and fanart

--- app/views/movieset-listing.blade.php
+++ app/views/movieset-listing.blade.php
@@ -5,12 +5,13 @@
 		<table>
 			<thead>
 				<tr>
-					<th>Name</th><th>Number of movies</th>
+					<th>Poster</th><th>Name</th><th>Number of movies</th>
 				</tr>
 			</thead>
 
 			@foreach($movieSets as $set)
 				<tr>
+					<td><a href="/movieset/{{$set->idSet}}"><img width="100px" src="{{$set->poster()}}"/></a></td>
 					<td><a href="/movieset/{{$set->idSet}}" >{{ $set->getName() }}</a></td>
 					<td>{{$set->movies->count()}}</td>
 				</tr>
